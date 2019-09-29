e1d6d036bb390beff8e719f6d095927e0650f1ae
suddin0 <suddin@student.42.fr>
2019-09-29 14:26:07 +0200

    Must finish the getopt_error
    Signed-off-by: suddin0 <suddin@student.42.fr>

--- src/getopt_src/getopt_end.c
+++ src/getopt_src/getopt_end.c
@@ -16,7 +16,11 @@ int	getopt_end(t_getopt_internal_args arg)
 {
 	if (arg.opt_treat == OPT_TREAT_MUTATE)
 		arg.getopt_data->optind = arg.getopt_data->last_optind;
-	else
-		arg.getopt_data->optind++;
+	else if (arg.opt_treat == OPT_TREAT_POSIXLY_CORRECT)
+	{
+		printf("CAME HERE %d, %d\n", arg.getopt_data->optind, arg.getopt_data->last_optind);
+
+	}
+		// arg.getopt_data->optind;
 	return (GETOPT_END);
 }
